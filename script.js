c29e29b6052e2787245dc52c0296c593eafad7b4
yy <yuyue9284@126.com>
2017-03-10 18:06:01 -0800

    ver 3 premium cat clicker

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-var ct = [0, 0];
-$("img").click(function() {
-	var cid = Number($(this).attr("id"));
-	ct[cid] += 1;
-	$(this).next().find(".count").text(ct[cid]);
+var numberofcats = 3;
+var ct = [];
+var list = $("#catlist");
 
-	console.log(ct);
-	console.log(cid);
+for (var i = 0; i < numberofcats; i++) {
+    ct.push(0);
+    var s = "<a><li cid=" + i + ">cat " + (i + 1) + "</li></a>";
+    list.append(s);
+};
+
+var cur = undefined;
+
+$("li").click(function() {
+    var cid = Number($(this).attr("cid"));
+    if (cur === undefined) {
+        cur = cid;
+    } else {
+        $(".content[cid = " + cur + "]").css("display", "none");
+        cur = cid;
+    }
+    $(".content[cid = " + cur + "]").css("display", "block");
+});
+
+$("img").click(function() {
+    var cid = Number($(this).attr("id"));
+    ct[cid] += 1;
+    $(this).next().find(".count").text(ct[cid]);
+    console.log(cid);
 });
