543de2d1f55e966e46a36ebae8a6308999bbaefa
yy <yuyue9284@126.com>
2017-03-10 16:56:24 -0800

    ver 2

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var ct = 0;
-
+var ct = [0, 0];
 $("img").click(function() {
-	ct += 1;
-	$("#count").text(ct);
+	var cid = Number($(this).attr("id"));
+	ct[cid] += 1;
+	$(this).next().find(".count").text(ct[cid]);
+
+	console.log(ct);
+	console.log(cid);
 });
