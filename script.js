6d6f92d7fb7d3c7497d4b3bce5a424a83f075363
yy <yuyue9284@126.com>
2017-03-13 16:52:12 -0700

    rewrite codes according to MVC

--- script.js
+++ script.js
@@ -1,29 +1,79 @@
-var numberofcats = 3;
-var ct = [];
-var list = $("#catlist");
+$(function() {
+    var model = {
+        init: function() {
+            if (!localStorage.cats) {
+                var cats = [{
+                    cid: 0,
+                    name: "Cat1",
+                    count: 0,
+                    src: "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426"
+                }, {
+                    cid: 1,
+                    name: "Cat2",
+                    count: 1,
+                    src: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496"
+                }, {
+                    cid: 2,
+                    name: "Cat3",
+                    count: 2,
+                    src: "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454"
+                }]
+                localStorage.cats = JSON.stringify(cats);
+            }
+        },
 
-for (var i = 0; i < numberofcats; i++) {
-    ct.push(0);
-    var s = "<a><li cid=" + i + ">cat " + (i + 1) + "</li></a>";
-    list.append(s);
-};
+        addcount: function(cid) {
+            var catlist = JSON.parse(localStorage.cats);
+            catlist[cid].count += 1;
+            localStorage.cats = JSON.stringify(catlist);
+            return catlist[cid];
+        },
 
-var cur = undefined;
+        getcat: function(cid) {
+            var catlist = JSON.parse(localStorage.cats);
+            return catlist[cid];
+        },
 
-$("li").click(function() {
-    var cid = Number($(this).attr("cid"));
-    if (cur === undefined) {
-        cur = cid;
-    } else {
-        $(".content[cid = " + cur + "]").css("display", "none");
-        cur = cid;
-    }
-    $(".content[cid = " + cur + "]").css("display", "block");
-});
+        getcatlist: function() {
+            return JSON.parse(localStorage.cats);
+        }
+    };
 
-$("img").click(function() {
-    var cid = Number($(this).attr("id"));
-    ct[cid] += 1;
-    $(this).next().find(".count").text(ct[cid]);
-    console.log(cid);
+    var view = {
+        renderlist: function(catlist) {
+            catlist.forEach(function(cat) {
+                $("#catlist").append("<li cid = \"" + cat.cid + "\">" + cat.name + "</li>")
+            })
+        },
+
+        rendercat: function(cat) {
+            $("#name").html(cat.name);
+            $('#count').html(cat.count);
+            $("#img").attr({ "src": cat.src, "cid": cat.cid });
+            $(".cats").css("display", "block");
+        }
+    };
+
+    var controller = {
+        init: function() {
+            model.init();
+            view.renderlist(model.getcatlist());
+            $("li").click(this.showcats);
+            $("img").click(this.increase);
+        },
+
+        increase: function() {
+            var cid = $(this).attr("cid");
+            var cat = model.addcount(cid);
+            view.rendercat(cat);
+        },
+
+        showcats: function() {
+            var cid = $(this).attr("cid");
+            var cat = model.getcat(Number(cid));
+            view.rendercat(cat);
+        }
+    };
+
+    controller.init();
 });
